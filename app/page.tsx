24ca133c656c493f9b5a3a2f12135e8a0a0863d3
Linda Zheng <lindazheng1993@gmail.com>
2023-08-06 05:52:27 -0500

    updating some prettier and eslint stuff

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,34 @@ export default function Home() {
     return (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
             <div className="flex flex-col max-w-lg text-center justify-center gap-4">
-                <h1 className={title({size: 'lg'})}>
-                    Play
+                <span>
+                    <h1 className={title({size: 'lg'})}>
+                        Play
+                    </h1>
                     <h1 className={title({
                         size: 'lg',
                         color: 'green'
-                    })}> games</h1>
-                </h1>
+                    })}
+                    >
+                        games
+                    </h1>
+                </span>
+
                 <h1 className={title({size: 'lg'})}>with friends</h1>
                 <h1 className={title({size: 'lg'})}>online.</h1>
-                <h2 className={subtitle({class: 'mt-4'})}>
-                    Powered by
+
+                <span>
+                    <h2 className={subtitle({class: 'mt-4'})}>
+                        Powered by
+                    </h2>
                     <h2 className={subtitle({
                         class: 'mt-4 font-bold',
                         color: 'violet'
-                    })}> GPT</h2>
-                </h2>
+                    })}
+                    >
+                        GPT
+                    </h2>
+                </span>
             </div>
         </section>
     );
