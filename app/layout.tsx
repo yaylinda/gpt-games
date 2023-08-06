72cd699330eaaed58620d5da158b6dcc119e9060
Linda Zheng <lindazheng1993@gmail.com>
2023-08-06 05:56:08 -0500

    update special html symbols

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,66 @@
 import AuthModal from '@/app/AuthModal';
-import {Navbar} from '@/app/Navbar';
-import {fontSans} from '@/config/fonts';
-import {siteConfig} from '@/config/site';
+import { Navbar } from '@/app/Navbar';
+import { fontSans } from '@/config/fonts';
+import { siteConfig } from '@/config/site';
 import '@/styles/globals.css';
 import clsx from 'clsx';
-import {Metadata} from 'next';
-import {Providers} from './providers';
+import moment from 'moment';
+import { Metadata } from 'next';
+import { Providers } from './providers';
 
 export const metadata: Metadata = {
     title: {
-        default: siteConfig.name, template: `%s - ${siteConfig.name}`,
+        default: siteConfig.name,
+        template: `%s - ${siteConfig.name}`,
     },
     description: siteConfig.description,
-    themeColor: [{media: '(prefers-color-scheme: light)', color: 'white'},
-        {media: '(prefers-color-scheme: dark)', color: 'black'},],
+    themeColor: [
+        {
+            media: '(prefers-color-scheme: light)',
+            color: 'white',
+        },
+        {
+            media: '(prefers-color-scheme: dark)',
+            color: 'black',
+        },
+    ],
     icons: {
-        icon: '/favicon.ico', shortcut: '/favicon-16x16.png', apple: '/apple-touch-icon.png',
+        icon: '/favicon.ico',
+        shortcut: '/favicon-16x16.png',
+        apple: '/apple-touch-icon.png',
     },
 };
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
-        <head/>
-        <body
-            className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}
-        >
-        <Providers themeProps={{attribute: 'class', defaultTheme: 'dark'}}>
-            <div className="relative flex flex-col h-screen">
-                <Navbar/>
-                <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-                    {children}
-                </main>
-                <footer className="w-full flex items-center justify-center py-3">
-                    <p className="text-sm text-default-400">(c) 2023 YayLinda, Inc.</p>
-                </footer>
-            </div>
-            <AuthModal/>
-        </Providers>
-        </body>
+            <head />
+            <body
+                className={clsx(
+                    'min-h-screen bg-background font-sans antialiased',
+                    fontSans.variable
+                )}
+            >
+                <Providers
+                    themeProps={{
+                        attribute: 'class',
+                        defaultTheme: 'dark',
+                    }}
+                >
+                    <div className="relative flex flex-col h-screen">
+                        <Navbar />
+                        <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+                            {children}
+                        </main>
+                        <footer className="w-full flex items-center justify-center py-3">
+                            <p className="text-sm text-default-400">
+                                &copy; {moment().year()} YayLinda Inc.
+                            </p>
+                        </footer>
+                    </div>
+                    <AuthModal />
+                </Providers>
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,31 @@
-import {subtitle, title} from '@/components/primitives';
+import { subtitle, title } from '@/components/primitives';
 
 export default function Home() {
     return (
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
             <div className="flex flex-col max-w-lg text-center justify-center gap-4">
                 <span>
-                    <h1 className={title({size: 'lg'})}>
-                        Play
-                    </h1>
-                    <h1 className={title({
-                        size: 'lg',
-                        color: 'green'
-                    })}
+                    <h1 className={title({ size: 'lg' })}>Play&nbsp;</h1>
+                    <h1
+                        className={title({
+                            size: 'lg',
+                            color: 'green',
+                        })}
                     >
                         games
                     </h1>
                 </span>
 
-                <h1 className={title({size: 'lg'})}>with friends</h1>
-                <h1 className={title({size: 'lg'})}>online.</h1>
+                <h1 className={title({ size: 'lg' })}>with friends</h1>
+                <h1 className={title({ size: 'lg' })}>online.</h1>
 
                 <span>
-                    <h2 className={subtitle({class: 'mt-4'})}>
-                        Powered by
-                    </h2>
-                    <h2 className={subtitle({
-                        class: 'mt-4 font-bold',
-                        color: 'violet'
-                    })}
+                    <h2 className={subtitle({ class: 'mt-4' })}>Powered by&nbsp;</h2>
+                    <h2
+                        className={subtitle({
+                            class: 'mt-4 font-bold',
+                            color: 'violet',
+                        })}
                     >
                         GPT
                     </h2>
