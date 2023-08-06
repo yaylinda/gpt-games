3ff0578834d9060f11a4fbdb1ee018f96026f1d3
Linda Zheng <lindazheng1993@gmail.com>
2023-08-06 01:25:11 -0500

    add nextui component library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import './globals.css'
+import {Providers} from '@/app/providers';
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'GPT Games',
-  description: 'Play games together (powered by GPT)',
+    title: 'GPT Games',
+    description: 'Play games together (powered by GPT)',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en" className="dark">
+        <body>
+        <Providers>
+            {children}
+        </Providers>
+        </body>
+        </html>
+    )
 }
